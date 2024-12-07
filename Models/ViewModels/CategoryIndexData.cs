e2c02bb5a611a86dff8bacc0b29dc4137eda3498
BOGDAN-IONUȚ CICIOVAN <bogdan.ciciovan@stud.ubbcluj.ro>
2024-12-07 17:27:49 +0200

    Rezolvare laborator 4

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Ciciovan_Bogdan_Ionut_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+    }
+}
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Ciciovan_Bogdan_Ionut_Lab2.Models;
+
+namespace Ciciovan_Bogdan_Ionut_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
