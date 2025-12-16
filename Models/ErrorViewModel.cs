701a72985eb624862be8c716286894dfe94cc72b
BOGDAN-IONUȚ CICIOVAN <bogdan.ciciovan@stud.ubbcluj.ro>
2025-12-16 17:53:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ciciovan_Bogdan_Ionut_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
